06c279709824f7caef56680a76a24bf228402eee
gabisaka <gabrielayts@hotmail.com>
2022-06-06 08:00:39 -0400

    Lendo arquivo de texto

--- forca.cpp
+++ forca.cpp
@@ -2,6 +2,7 @@
 #include <string> 
 #include <map>
 #include <vector>
+#include <fstream>
 using namespace std; 
 
 const string PALAVRA_SECRETA = "MELANCIA";
@@ -76,9 +77,27 @@ void chuta () {
     cout << endl;
 }
 
+void le_arquivo () {
+    ifstream arquivo;
+    arquivo.open("palavras.txt");
+
+    int quantidade_palavras;
+    arquivo >> quantidade_palavras;
+
+    cout << "O arquivo possui " << quantidade_palavras << " palavras." << endl;
+
+    for (int i=0; i < quantidade_palavras; i++) {
+      string palavra_lida; 
+      arquivo >> palavra_lida; 
+      cout << "Na linha " << i << ":" << palavra_lida << endl;
+    }
+}
+
 int main () {
     
     imprime_cabecalho();
+
+    le_arquivo();
 
     while(nao_acertou() && nao_enforcou()){
         
